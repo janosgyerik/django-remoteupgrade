2d6a4751a1e60933d19c68e02a306e1a14060b9c
Janos Gyerik <info@janosgyerik.com>
2013-10-13 18:03:30 +0200

    moved gh_post_receive one level higher

--- demo/manage.py
+++ demo/manage.py
@@ -2,6 +2,9 @@
 import os
 import sys
 
+DIRNAME = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.join(DIRNAME, '..'))
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo.settings")
 
