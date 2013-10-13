aebab0bca8a58c8fe854c317f01095f40ea30471
Janos Gyerik <info@janosgyerik.com>
2013-10-13 18:14:45 +0200

    added convenience scripts to run the demo and unit tests

---
+++ demo/manage.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd $(dirname "$0")
+
+projectname=redeployer
+virtualenv=~/virtualenv/$projectname
+test -d $virtualenv && . $virtualenv/bin/activate || {
+    echo virtualenv does not exist: $virtualenv
+    echo Create it with: virtualenv --distribute $virtualenv
+    exit 1
+}
+
+if test $# = 0; then
+    python manage.py help
+else
+    python manage.py $*
+fi
---
+++ demo/unit.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd $(dirname "$0")
+./manage.sh test redeployer
